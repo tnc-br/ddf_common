7c27b735177c994bb797a01aebb681fe67fbfec0
erickzul <137205035+erickzul@users.noreply.github.com>
2023-08-02 21:15:49 +0000

    Add get_predictions method

--- hypothesis.py
+++ hypothesis.py
@@ -77,6 +77,56 @@ def sample_ttest(longitude: float,
 
     return HypothesisTest(longitude, latitude, p_value, p_value_target)
 
+def get_predictions(sample_data: pd.DataFrame,
+                    isotope_column_names: list[str],
+                    means_isoscapes: list[raster.AmazonGeoTiff],
+                    variances_isoscapes: list[raster.AmazonGeoTiff],
+                    sample_size_per_location: int):
+  '''
+  Calculates the accuracy, precision, recall based on true positives and negatives,
+  and the false positive and negatives. (go/ddf-glossary)
+
+  sample_data: pd.DataFrame with lat, long, isotope_value and fraudulent columns
+  means_isoscape: Isoscape that maps geographic coordinates to a mean isotope value.
+  variances_isoscape: Isoscape that maps geographic coordinates to the variance of
+                      isotope valuesat that location.
+  sample_size_per_location: Number of samples per geographic location used to calculate
+                            mean and variance in isoscapes.
+  p_value_target: desired p_value for the t-test (e.sample_data: 0.05)
+  '''
+  sample_data = sample_data.groupby([
+      _TREE_CODE_COLUMN_NAME,
+      _LONGITUDE_COLUMN_NAME,
+      _LATITUDE_COLUMN_NAME,
+      _FRAUDULENT_COLUMN_NAME])[isotope_column_names]
+  predictions = pd.DataFrame()
+
+  for group_key, isotope_values in sample_data:
+    if isotope_values.shape[0] <= 1:
+      continue
+
+    p_values = []
+    for i, isotope_column_name in enumerate(isotope_column_names):
+      hypothesis_test = sample_ttest(longitude=group_key[1],
+                                     latitude=group_key[2],
+                                     isotope_values=isotope_values[isotope_column_name],
+                                     means_isoscape=means_isoscapes[i],
+                                     variances_isoscape=variances_isoscapes[i],
+                                     sample_size_per_location=sample_size_per_location,
+                                     p_value_target=None)
+      p_values.append(hypothesis_test.p_value)
+    combined_p_value = np.array(p_values).prod()
+    if np.isnan(combined_p_value):
+      continue
+
+    row = {"Code": [group_key[0]],
+            "lat": [group_key[1]],
+            "long": [group_key[2]],
+            "fraud": [group_key[3]],
+            "predicted_fraud": [combined_p_value]}
+    predictions = pd.concat([predictions, pd.DataFrame(row)], ignore_index=True)
+
+  return predictions
 
 def fraud_metrics(sample_data: pd.DataFrame,
                   isotope_column_names: list[str],
@@ -124,6 +174,8 @@ def fraud_metrics(sample_data: pd.DataFrame,
                                        p_value_target)
         p_values.append(hypothesis_test.p_value)
       combined_p_value = np.array(p_values).prod()
+      if np.isnan(combined_p_value):
+        continue
 
       if not group_key[3]:
         if combined_p_value >= p_value_target:
