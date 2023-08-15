8452681b5cfafff52f5e2ae119a1bae2142b3df4
Ruben Madera <tripiace@yahoo.com>
2023-08-15 14:14:42 -0400

    Add self parameters to member functions

--- model.py
+++ model.py
@@ -7,7 +7,7 @@ class Model:
     def __init__(self) -> None:
         pass
     
-    def predict_on_batch(X: pd.DataFrame):
+    def predict_on_batch(self, X: pd.DataFrame):
         pass
 
 class TFModel(Model):
@@ -16,15 +16,15 @@ class TFModel(Model):
         self.vi_model =  self.load_tf_model(tf_model_path)
         self.transformer = self.apply_transformer(transformer_path)
 
-    def load_transformer(transformer_path: str) -> ColumnTransformer:
+    def load_transformer(self, transformer_path: str) -> ColumnTransformer:
         return joblib.load(transformer_path)
     
-    def apply_transformer(X: pd.DataFrame) -> pd.DataFrame:
+    def apply_transformer(self, X: pd.DataFrame) -> pd.DataFrame:
         return pd.DataFrame(self.transformer.transform(X),
                             index=X.index, columns=X.columns)
     
-    def load_tf_model(tf_model_path: str) -> tf.keras.Model:
+    def load_tf_model(self, tf_model_path: str) -> tf.keras.Model:
         return tf.keras.saving.load_model(tf_model_path)
     
-    def predict_on_batch(X: pd.DataFrame):
+    def predict_on_batch(self, X: pd.DataFrame):
         return self.vi_model.predict_on_batch(X)
