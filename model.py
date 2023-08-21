5a56a39c3924ea7d7ec57240f807a666621df4eb
Ruben Madera <tripiace@yahoo.com>
2023-08-21 16:29:49 -0400

    Make some functions private

--- model.py
+++ model.py
@@ -13,19 +13,19 @@ class Model:
 class TFModel(Model):
     def __init__(self, tf_model_path: str, transformer_path: str) -> None:
         Model.__init__(self)
-        self.vi_model =  self.load_tf_model(tf_model_path)
-        self.transformer = self.load_transformer(transformer_path)
+        self.vi_model =  self._load_tf_model(tf_model_path)
+        self.transformer = self._load_transformer(transformer_path)
 
-    def load_transformer(self, transformer_path: str) -> ColumnTransformer:
+    def _load_transformer(self, transformer_path: str) -> ColumnTransformer:
         return joblib.load(transformer_path)
     
-    def apply_transformer(self, X: pd.DataFrame) -> pd.DataFrame:
+    def _apply_transformer(self, X: pd.DataFrame) -> pd.DataFrame:
         return pd.DataFrame(self.transformer.transform(X),
                             index=X.index, columns=X.columns)
     
-    def load_tf_model(self, tf_model_path: str) -> tf.keras.Model:
+    def _load_tf_model(self, tf_model_path: str) -> tf.keras.Model:
         return tf.keras.saving.load_model(tf_model_path)
     
     def predict_on_batch(self, X: pd.DataFrame):
-        X = self.apply_transformer(X)
+        X = self._apply_transformer(X)
         return self.vi_model.predict_on_batch(X)
