df192e36ff7b6ca85317d1212230f57de42873a5
Ruben Madera <tripiace@yahoo.com>
2023-08-21 16:58:54 -0400

    Add doc comments and convert some methods to abstract

--- model.py
+++ model.py
@@ -1,16 +1,30 @@
 from sklearn.compose import ColumnTransformer
+from abc import abstractmethod
 import tensorflow as tf
 import joblib
 import pandas as pd
 
 class Model:
+    '''
+    Abstract class representing a model, trained in any way.
+    '''
+
+    @abstractmethod
     def __init__(self) -> None:
         pass
     
+    @abstractmethod
     def predict_on_batch(self, X: pd.DataFrame):
+        '''
+        Makes a prediction on a collection of inputs.
+        '''
         pass
 
 class TFModel(Model):
+    '''
+    A TF trained model with scaled input training data. The scalers
+    are stored in `transformer` and are applied to input before predictions.
+    '''
     def __init__(self, tf_model_path: str, transformer_path: str) -> None:
         Model.__init__(self)
         self.vi_model =  self._load_tf_model(tf_model_path)
