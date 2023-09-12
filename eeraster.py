3a84bbf86cc6254af934746792c6f3b207db15d9
Ruben Madera <tripiace@yahoo.com>
2023-09-11 21:12:16 -0400

    Implement new function to ingest isoscapes into earth engine. Add callbacks to stamp them with metadata.

--- eeraster.py
+++ eeraster.py
@@ -4,7 +4,7 @@ import ee
 import eeddf
 import math
 import raster
-from typing import Tuple, List
+from typing import Tuple, List, Dict
 from multiprocessing import Pool
 
 
@@ -15,6 +15,8 @@ _LATITUDE_COLUMN_NAME = "lat"
 _CRS = 'EPSG:3857'
 _demXfab = None
 _dem = None
+_BUCKET_NAME = "unprocessed-isoscapes"
+DEFAULT_OXYGEN_EE_ASSET = "projects/river-sky-386919/assets/isoscapes/d18O_isocape"
 
 #global pool for all rasters in cases of simultaneous parallelization
 _pool = None
@@ -129,6 +131,104 @@ class eeRaster(raster.AmazonGeoTiffBase):
 
       all_dfs = _getPool().starmap(_query_mp, query_list)
       return pd.concat(all_dfs, ignore_index=True)
+
+def stamp_isoscape(ee_asset_path: str, properties: Dict[str, typing.Any]):
+  """
+  stamp_isoscape function
+  ------------------------
+  This function stamps an Earth Engine isoscape with properties.
+  This function can also be used to edit an existing stamp.
+  ------------------------
+  Parameters:
+  ee_asset_path : str
+      GeoTIFF filename in Earth Engine (with the full path) of the isoscape to be stamped
+      e.g: projects/river-sky-386919/assets/isoscapes/d18O_isoscape
+  properties: Dict[str, Any]
+      A dictionary of metadata to apply to the Earth Engine asset
+      e.g. {"p_value" : 0.95}
+  """
+  eeddf.initialize_ddf()
+  isoscape = ee.Image(f"{EE_ASSET_FOLDER}/{ee_asset_path}")
+  isoscape.set(var_args=properties)
+
+def show_stamps(ee_asset_path: str):
+  
+  """
+  show_stamps function
+  ------------------------
+  This function returns all the properties on an Earth Engine isoscape
+  ------------------------
+  Parameter:
+  ee_asset_path : str
+      GeoTIFF filename in Earth Engine (with the full path) of the isoscape to be stamped
+      e.g: projects/river-sky-386919/assets/isoscapes/d18O_isoscape
+  """
+  eeddf.initialize_ddf()
+  isoscape = ee.Image(ee_asset_path)
+  return isoscape.getInfo().properties()
+
+def del_stamp(filename: str, property_name: str):
+  """
+  del_stamp function
+  ------------------------
+  This function deletes a property from an Earth Engine isoscape
+  ------------------------
+  Parameters:
+  filename : str
+      GeoTIFF filename (with the full path) of the stamped isoscape
+      e.g: projects/river-sky-386919/assets/isoscapes/d18O_isoscape
+  property_name : str
+      Name of the metadata to be deleted
+      e.g: "p_value"
+  """  
+  eeddf.initialize_ddf()
+  isoscape = ee.Image(e_asset_path)
+  isoscape.set(property_key, None)
+
+def ingest_isoscape(
+    isoscape_path: str,
+    properties: Dict[str, typing.Any],
+    ee_dst_path: str=DEFAULT_OXYGEN_EE_ASSET,
+    allow_overwrite: bool=False):
+  """
+  ingest_isoscape function
+  ------------------------
+  Ingests the isoscape in earth engine, and stamps it with `properties`. Requires access
+  to the "unprocessed-isoscape" cloud storage bucket and to the EE API for project river-sky-386919.
+  ------------------------
+  Parameters:
+  filename : isoscape_path
+      GeoTIFF filename (with the full path) of the stamped isoscape
+      e.g: /content/gdrive/Shared drives/TNC Fellowship 🌳/4. Isotope Research & Signals/code/amazon_rainforest_files/amazon_rasters/variational/ensemble_with_carbon_brisoisorix/fixed_isorix_carbon_ensemble.tiff
+  properties: Dict[str, Any]
+      A dictionary of metadata to apply to the Earth Engine asset
+      e.g. {"p_value" : 0.95}
+  ee_dst_path: Path of the new asset to create in Earth Engine.
+      e.g.  projects/river-sky-386919/assets/staging/test_isoscape
+  allow_overwrite: bool
+      If true, allows user to overwrite an existing isoscape with the same filename and ee_dst_path. 
+  """  
+  eeddf.initialize_ddf()
+
+  # 1. Upload to Google Cloud Storage. This is unfortuntaly required to upload to EE using their API.
+  storage_client = storage.Client()
+  
+  global _BUCKET_NAME
+  bucket = storage_client.bucket(_BUCKET_NAME)
+
+  _, isoscape_filename = os.path.split(isoscape_path)
+  blob = bucket.blob(isoscape_filename)
+  
+  blob.upload_from_filename(isoscape_path, if_generation_match=(1 if allow_overwrite else None))
+
+  # 2. Copy the file from Google Cloud Storage to Earth Engine. 
+  request_id = ee.data.newTaskId()[0]
+  params = {
+    "name": ee_dst_path,
+    "tilesets": [{"sources": [{"uris": [f"gs://{_BUCKET_NAME}/{isoscape_filename}"]}]}]
+  }
+  ee.data.startIngestion(request_id=ee_request_id, params=params,
+                         callback=stamp_isoscape(ee_dst_path, metadata))
 
 def demXfab():
   """
