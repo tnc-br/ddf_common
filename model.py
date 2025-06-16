a8e6d6a2595000f418ec04f369eec8c5660a89d8
Ruben Madera <tripiace@yahoo.com>
2025-06-16 15:36:30 -0400

    wrapped

--- model.py
+++ model.py
@@ -186,11 +186,20 @@ def get_checkpoint_callback(model_file):
       model_file,
       monitor='val_loss', verbose=0, save_best_only=True, mode='min')
 
-# You could use:
 @tf.keras.utils.register_keras_serializable()
 class SoftplusLayer(tf.keras.layers.Layer):
     def call(self, inputs):
         return tf.math.log(1 + tf.exp(inputs))
+
+class TFPIndependentNormalWrapper(layers.Layer):
+    def __init__(self, event_shape, name="normal_dist_wrapper", **kwargs):
+        super().__init__(name=name, **kwargs)
+        # We create the TFP layer instance *inside* our custom layer's constructor.
+        self.distribution_layer = tfp.layers.IndependentNormal(event_shape=event_shape)
+
+    def call(self, inputs):
+        # The forward pass of our wrapper simply calls the TFP layer.
+        return self.distribution_layer(inputs)
 
 # The loss function is the negative log-likelihood
 def negative_log_likelihood(y_true, y_predicted_dist):
@@ -306,7 +315,7 @@ def train_or_update_variational_model(
             # # Concatenate the final loc and scale parameters back together.
             # params = layers.Concatenate(name='final_params')([loc_output, scale_output])
 
-            outputs = tfp.layers.IndependentNormal(event_shape=1, name='normal_distribution')(params)
+            outputs = TFPIndependentNormalWrapper(event_shape=1, name='wrapped_normal_distribution')(params)
 
             model = keras.Model(inputs=inputs, outputs=outputs)
 
